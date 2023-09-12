d15c2551e6615ec2ee3aa369aa07aab6bcceaa6a
Yurick <YJakse789@gmail.com>
2023-09-11 23:24:26 -0400

    Completed Lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,6 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="Example",breed="Pug"):
+        self.name = name.title() if type(name).__name__ == "str" and len(name) < 25 and len(name) > 1 else print("Name must be string between 1 and 25 characters.")
+        self.breed = breed if APPROVED_BREEDS.count(breed) == 1 else print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,6 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Example",job="ITC"):
+        self.name = name.title() if type(name).__name__ == "str" and len(name) < 25 and len(name) > 1 else print("Name must be string between 1 and 25 characters.")
+        self.job = job if APPROVED_JOBS.count(job) == 1 else print("Job must be in list of approved jobs.")
